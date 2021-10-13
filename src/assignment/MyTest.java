3ba680ca9ba833d37ab3dc4ab9e6a455d73f39ce
Wil Merchant <wtmerchant3@gmail.com>
2021-10-12 22:14:44 -0400

    Added code to test scenario for displaying a table successfully.

--- src/assignment/MyTest.java
+++ src/assignment/MyTest.java
@@ -1,37 +1,46 @@
 package assignment;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class MyTest {
 	public static void main(String[] args) {
         // declaration and instantiation of objects/variables
-//    	System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
-//		WebDriver driver = new FirefoxDriver();
 		//comment the above 2 lines and uncomment below 2 lines to use Chrome
 		System.setProperty("webdriver.chrome.driver","resources/chromedriver");
 		WebDriver driver = new ChromeDriver();
-    	
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         String baseUrl = "https://codepen.io/CalendlyQA/full/KKPQLmV";
-        String expectedTitle = "Welcome: Mercury Tours";
-        String actualTitle = "";
-
-        // launch Fire fox and direct it to the Base URL
+        
+        // launch Chrome and direct it to the Base URL
         driver.get(baseUrl);
-
-//        // get the actual value of the title
-//        actualTitle = driver.getTitle();
-
-        /*
-         * compare the actual title of the page with the expected one and print
-         * the result as "Passed" or "Failed"
-         */
-//        if (actualTitle.contentEquals(expectedTitle)){
-//            System.out.println("Test Passed!");
-//        } else {
-//            System.out.println("Test Failed");
-//        }
-       
+        
+        
+        driver.switchTo().frame("result");
+        
+        WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
+        inputField.clear();
+        inputField.sendKeys("3");
+        
+        
+        WebElement playButton = driver.findElement(By.tagName("button"));
+        playButton.click();
+        
+//        WebDriverWait wait = new WebDriverWait(driver,30);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='0']")));
+        
+        
+        WebElement firstSquare = driver.findElement(By.xpath("//*[@id='0']"));
+        if(firstSquare.isDisplayed()) {
+        	System.out.println("The table is displayed.");
+        }
+        
         //close Fire fox
         driver.close();
        
