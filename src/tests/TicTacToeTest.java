2deaad836f403c6bef15b7c632e98ae0d43a67e4
Wil Merchant <wtmerchant3@gmail.com>
2021-10-15 01:45:52 -0400

    Added all of the tests to the Test Class

--- src/tests/TicTacToeTest.java
+++ src/tests/TicTacToeTest.java
@@ -4,6 +4,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -29,16 +31,28 @@ public class TicTacToeTest {
 	}
 	
 	@Test(testName = "Scenario 1")
-	public void verifyTableGeneratedWithCorrectInput() {
-        WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
+	public void verifyTableGeneratedWithCorrectInput() throws InterruptedException {
+		String inputValue = "3";
+		
+		System.out.println("Inputting value into input field - " + inputValue);
+		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
         inputField.clear();
-        inputField.sendKeys("3");
+        inputField.sendKeys(inputValue);
         
+        System.out.println("Clicking play button");
         WebElement playButton = driver.findElement(By.tagName("button"));
         playButton.click();
         
+        System.out.println("Checking to see if tic tac toe board generated");
         WebElement firstSquare = driver.findElement(By.xpath("//*[@id='0']"));
+        System.out.println("Is board generated: " + firstSquare.isDisplayed());
         
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         Assert.assertTrue(firstSquare.isDisplayed(), "Assert that the table is successfully generated.");
 	}
 	
@@ -48,15 +62,25 @@ public class TicTacToeTest {
 		int expectedNumberOfFirstSquares = 0;
 		String inputValue = "0";
 		
+		System.out.println("Inputting value into input field - " + inputValue);
 		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
         inputField.clear();
         inputField.sendKeys(inputValue);
         
+        System.out.println("Clicking play button");
         WebElement playButton = driver.findElement(By.tagName("button"));
         playButton.click();
         
+        System.out.println("Checking for the number of tic tac toe tables on the page.");
         List<WebElement> firstSquares = driver.findElements(By.xpath("//*[@id='0']"));
+        System.out.println("Number of tables: " + firstSquares.size());
         
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         Assert.assertEquals(firstSquares.size(), expectedNumberOfFirstSquares, "Assert that the table is not generated if user inputs a number less than or equal to zero.");
 	}
 	
@@ -65,14 +89,25 @@ public class TicTacToeTest {
 		int expectedNumberOfFirstSquares = 0;
 		String inputValue = "Q";
 		
+		System.out.println("Inputting value into input field - " + inputValue);
 		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
         inputField.clear();
         inputField.sendKeys(inputValue);
         
+        System.out.println("Clicking play button");
         WebElement playButton = driver.findElement(By.tagName("button"));
         playButton.click();
         
+        System.out.println("Checking for the number of tic tac toe tables on the page.");
         List<WebElement> firstSquares = driver.findElements(By.xpath("//*[@id='0']"));
+        System.out.println("Number of tables: " + firstSquares.size());
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         Assert.assertEquals(firstSquares.size(), expectedNumberOfFirstSquares, "Assert that the table is not generated if user inputs a number less than or equal to zero.");
 	}
@@ -82,19 +117,39 @@ public class TicTacToeTest {
 		int expectedNumberOfFirstSquares = 1;
 		String inputValue = "3";
 		
+		System.out.println("Inputting value into input field - " + inputValue);
 		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
         inputField.clear();
         inputField.sendKeys(inputValue);
         
+        System.out.println("Clicking play button");
         WebElement playButton = driver.findElement(By.tagName("button"));
         playButton.click();
         
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        System.out.println("Inputting value into input field - " + inputValue);
         inputField.clear();
         inputField.sendKeys(inputValue);
         
+        System.out.println("Clicking play button");
         playButton.click();
         
+        System.out.println("Checking for the number of tic tac toe tables on the page.");
         List<WebElement> firstSquares = driver.findElements(By.xpath("//*[@id='0']"));
+        System.out.println("Number of tables: " + firstSquares.size());
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         Assert.assertEquals(firstSquares.size(), expectedNumberOfFirstSquares, "Assert that another table is not generated if a game is already in session.");
 	}
@@ -103,26 +158,281 @@ public class TicTacToeTest {
 	@Test(testName = "Scenario 5")
 	public void verifyGameCanEndWithAWinner() {
 		String expectedBannerText = "Congratulations player X! You've won. Refresh to play again!";
-		String inputValue = "3";
+		String inputValue = "1";
 		
+		System.out.println("Inputting value into input field - " + inputValue);
 		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
         inputField.clear();
         inputField.sendKeys(inputValue);
         
+        System.out.println("Clicking play button");
         WebElement playButton = driver.findElement(By.tagName("button"));
         playButton.click();
         
+        System.out.println("Board Generated!");
         List<WebElement> boardSquares = driver.findElements(By.xpath("//td"));
-        boardSquares.get(0).click();
-        boardSquares.get(1).click();
-        boardSquares.get(3).click();
-        boardSquares.get(4).click();
-        boardSquares.get(6).click();
         
+        int[] clickOrder = {0};
+        System.out.println("Clicking Squares on Tic Tac Toe board...");
+        for(int i : clickOrder) {
+            System.out.println("Clicking space on row: " + boardSquares.get(i).getAttribute("data-row") + " column: " + boardSquares.get(i).getAttribute("data-column") +"." );
+            boardSquares.get(i).click();
+            System.out.println("Space value: " + boardSquares.get(i).getText());
+        }
+        
+        System.out.println("Reading text from end game banner....");
         WebElement endGameDiv = driver.findElement(By.id("endgame"));
         String actualText = endGameDiv.getText();
+        System.out.println("Banner text: "+ actualText);
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         Assert.assertEquals(actualText, expectedBannerText, "Assert that game ends with a winner and correct winner is displayed on banner");
+	}
+	
+	@Test(testName = "Scenario 6")
+	public void verifyGameCanEndInADraw() {
+		List<Boolean> expected = Arrays.asList(true, true, true, true, true, true, true, true, true);
+		List<Boolean> actual = new ArrayList();
+		String inputValue = "3";
+		
+		System.out.println("Inputting value into input field - " + inputValue);
+		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
+        inputField.clear();
+        inputField.sendKeys(inputValue);
+        
+        System.out.println("Clicking play button");
+        WebElement playButton = driver.findElement(By.tagName("button"));
+        playButton.click();
+		
+        System.out.println("Board Generated!");
+        
+        List<WebElement> boardSquares = driver.findElements(By.xpath("//td"));
+        int[] clickOrder = {0,4,1,3,5,2,6,7,8};
+        
+        System.out.println("Clicking Squares on Tic Tac Toe board...");
+        for(int i : clickOrder) {
+            System.out.println("Clicking space on row: " + boardSquares.get(i).getAttribute("data-row") + " column: " + boardSquares.get(i).getAttribute("data-column") +"." );
+            boardSquares.get(i).click();
+            System.out.println("Space value: " + boardSquares.get(i).getText());
+        }
+       
+        System.out.println("Checking if all squares on game board are filled");
+        boardSquares = driver.findElements(By.xpath("//td"));
+        for(WebElement square: boardSquares) {
+        	System.out.println("Is space on row: " + square.getAttribute("data-row") + " column: " + square.getAttribute("data-column") +" filled?");
+        	String squareValue = square.getText();
+        	System.out.println("Square value: " + squareValue);
+        	boolean isFilled = !squareValue.isEmpty();
+        	System.out.println("Result: " + isFilled);
+        	actual.add(isFilled);
+        }
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        Assert.assertEquals(actual, expected, "Assert game ends in draw when all spots on board are filled");
+	}
+	
+	@Test(testName = "Scenario 7")
+	public void verifySpacesCannotBeClickedAfterGameEndsInWinWithRemainingEmptySpaces() {
+		
+		String inputValue = "3";
+		int lastSpace = 7;
+		
+		System.out.println("Inputting value into input field - " + inputValue);
+		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
+        inputField.clear();
+        inputField.sendKeys(inputValue);
+        
+        System.out.println("Clicking play button");
+        WebElement playButton = driver.findElement(By.tagName("button"));
+        playButton.click();
+		
+        System.out.println("Board Generated!");
+        
+        List<WebElement> boardSquares = driver.findElements(By.xpath("//td"));
+        int[] clickOrder = {0,1,3,4,6};
+        
+        System.out.println("Clicking Squares on Tic Tac Toe board...");
+        for(int i : clickOrder) {
+            System.out.println("Clicking space on row: " + boardSquares.get(i).getAttribute("data-row") + " column: " + boardSquares.get(i).getAttribute("data-column") +"." );
+            boardSquares.get(i).click();
+            System.out.println("Space value: " + boardSquares.get(i).getText());
+        }
+        
+        System.out.println("Is end game banner displayed?");
+        WebElement endGameDiv = driver.findElement(By.id("endgame"));
+        System.out.println("Result: "+ endGameDiv.isDisplayed());
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        System.out.println("Clicking space on row: " + boardSquares.get(lastSpace).getAttribute("data-row") + " column: " + boardSquares.get(lastSpace).getAttribute("data-column") +"." );
+        boardSquares.get(lastSpace).click();
+        String spaceValue = boardSquares.get(lastSpace).getText();
+        System.out.println("Space Value: " + spaceValue);
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		Assert.assertTrue(spaceValue.isEmpty(), "Assert that clicking an empty space after a game has ended in a win with empty spaces remaining does not fill the space.");
+	}
+	
+	@Test(testName = "Scenario 8")
+	public void verifySpacesCannotBeClickedAfterGameEndsInDraw() {
+		
+		String inputValue = "3";
+		int lastSpace = 8;
+		String expectedValue = "X";
+		
+		System.out.println("Inputting value into input field - " + inputValue);
+		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
+        inputField.clear();
+        inputField.sendKeys(inputValue);
+        
+        System.out.println("Clicking play button");
+        WebElement playButton = driver.findElement(By.tagName("button"));
+        playButton.click();
+		
+        System.out.println("Board Generated!");
+        
+        List<WebElement> boardSquares = driver.findElements(By.xpath("//td"));
+        int[] clickOrder = {0,4,1,3,5,2,6,7,8};
+        
+        System.out.println("Clicking Squares on Tic Tac Toe board...");
+        for(int i : clickOrder) {
+            System.out.println("Clicking space on row: " + boardSquares.get(i).getAttribute("data-row") + " column: " + boardSquares.get(i).getAttribute("data-column") +"." );
+            boardSquares.get(i).click();
+            System.out.println("Space value: " + boardSquares.get(i).getText());
+        }
+        
+        System.out.println("Checking if all squares on game board are filled");
+        boardSquares = driver.findElements(By.xpath("//td"));
+        for(WebElement square: boardSquares) {
+        	System.out.println("Is space on row: " + square.getAttribute("data-row") + " column: " + square.getAttribute("data-column") +" filled?");
+        	String squareValue = square.getText();
+        	System.out.println("Square value: " + squareValue);
+        	boolean isFilled = !squareValue.isEmpty();
+        	System.out.println("Result: " + isFilled);
+        }
+        
+        
+        System.out.println("Clicking space on row: " + boardSquares.get(lastSpace).getAttribute("data-row") + " column: " + boardSquares.get(lastSpace).getAttribute("data-column") +"." );
+        boardSquares.get(lastSpace).click();
+        String spaceValue = boardSquares.get(lastSpace).getText();
+        System.out.println("Space Value: " + spaceValue);
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		Assert.assertTrue(spaceValue.equals(expectedValue), "Assert that clicking on any space after a game has ended in a draw does not change the space value.");
+	}
+	
+	@Test(testName = "Scenario 9")
+	public void verifyPlayerOCanWinGame() {
+		String expectedBannerText = "Congratulations player O! You've won. Refresh to play again!";
+		String inputValue = "3";
+		
+		System.out.println("Inputting value into input field - " + inputValue);
+		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
+        inputField.clear();
+        inputField.sendKeys(inputValue);
+        
+        System.out.println("Clicking play button");
+        WebElement playButton = driver.findElement(By.tagName("button"));
+        playButton.click();
+		
+        System.out.println("Board Generated!");
+        
+        List<WebElement> boardSquares = driver.findElements(By.xpath("//td"));
+        int[] clickOrder = {0,2,3,4,7,6};
+        
+        System.out.println("Clicking Squares on Tic Tac Toe board...");
+        for(int i : clickOrder) {
+            System.out.println("Clicking space on row: " + boardSquares.get(i).getAttribute("data-row") + " column: " + boardSquares.get(i).getAttribute("data-column") +"." );
+            boardSquares.get(i).click();
+            System.out.println("Space value: " + boardSquares.get(i).getText());
+        }
+        
+        System.out.println("Reading text from end game banner....");
+        WebElement endGameDiv = driver.findElement(By.id("endgame"));
+        String actualText = endGameDiv.getText();
+        System.out.println("Banner text: "+ actualText);
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        Assert.assertEquals(actualText, expectedBannerText, "Assert that Player O can win the game and that the endgame banner declares them as the winner.");
+	}
+	
+	@Test(testName = "Scenario 10")
+	public void verifyPlayerCantClickAlreadySelectedSquareDuringUnfinishedGame() {
+		String inputValue = "2";
+		int lastSpace = 0;
+		String expectedValue = "X";
+		
+		System.out.println("Inputting value into input field - " + inputValue);
+		WebElement inputField = driver.findElement(By.xpath("//form/input[@id='number']"));
+        inputField.clear();
+        inputField.sendKeys(inputValue);
+        
+        System.out.println("Clicking play button");
+        WebElement playButton = driver.findElement(By.tagName("button"));
+        playButton.click();
+		
+        System.out.println("Board Generated!");
+        
+        List<WebElement> boardSquares = driver.findElements(By.xpath("//td"));
+        int[] clickOrder = {0};
+        
+        System.out.println("Clicking Squares on Tic Tac Toe board...");
+        for(int i : clickOrder) {
+            System.out.println("Clicking space on row: " + boardSquares.get(i).getAttribute("data-row") + " column: " + boardSquares.get(i).getAttribute("data-column") +"." );
+            boardSquares.get(i).click();
+            System.out.println("Space value: " + boardSquares.get(i).getText());
+        }
+        
+        
+        System.out.println("Clicking space on row: " + boardSquares.get(lastSpace).getAttribute("data-row") + " column: " + boardSquares.get(lastSpace).getAttribute("data-column") +"." );
+        boardSquares.get(lastSpace).click();
+        String spaceValue = boardSquares.get(lastSpace).getText();
+        System.out.println("Space Value: " + spaceValue);
+        
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		Assert.assertTrue(spaceValue.equals(expectedValue), "Assert that clicking on a space after it has already been clicked on by another user doesn't change its value during a currently running game.");
+		
 	}
 	
 	@AfterMethod()
